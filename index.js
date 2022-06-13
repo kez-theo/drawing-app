0dc4aa6043878c384b5c9659c658c25da4374c57
keranie theodosiou <keranie.theodosiou@gmail.com>
2022-06-13 11:18:18 -0400

    server and three.js boilerplate

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
+
+app.use('/', router)
+
+app.use(express.static(path.join(__dirname, './public')))
+
+router.use(function (req, res, next) {
+  const err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log(`listening on port ${port}`);
+});
+
+module.exports = router;
